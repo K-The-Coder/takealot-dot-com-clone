0e33e660433254ad86353277d517914e4424eac5
Keketso Mabule <kekemabule19990216@gmail.com>
2025-10-27 17:45:07 +0200

    added dropdown, search-bar, and tab-list to subheader

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,8 +1,54 @@
+import {
+  faAngleDown,
+  faChevronRight,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { departmentList, tabsList } from "../lists";
+
 export default function SubHeader() {
   return (
     <>
       <header className="sub-header">
-        
+        <div className="sub-header-container">
+          <div className="department-panel">
+            <div className="department-header">
+              <p>Shop By Department <FontAwesomeIcon className="down-arrow" icon={faAngleDown} /></p>
+            </div>
+            <div className="dropdown-list">
+              <ul className="department-list">
+                {departmentList.map((department) => (
+                  <li key={department}>
+                    {department} <FontAwesomeIcon className="right-arrow" icon={faChevronRight} />{" "}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="search-and-tabs-panel">
+            <form className="search-bar">
+              <input
+                type="search"
+                name="search"
+                id="search-input"
+                placeholder="Search for products, brands, etc"
+              />
+              <button type="submit">
+                <FontAwesomeIcon
+                  className="search-icon"
+                  icon={faMagnifyingGlass}
+                />
+              </button>
+            </form>
+            <div className="tabs-bar">
+              <ul className="tabs-list">
+                {tabsList.map((tabItem) => (
+                  <li key={tabItem}>{tabItem}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
       </header>
     </>
   );
---
+++ src/lists.ts
@@ -0,0 +1,21 @@
+export const tabsList: string[] = [
+  "ALOT For Less",
+  "New Arrivals",
+  "Christmas",
+  "Summer",
+  "Month of Sport",
+  "GetUp",
+  "Brands Store",
+  "Deals & Promotions",
+  "TakealotMORE",
+  "Clearance"
+];
+
+export const departmentList: string[] = [
+  "Appliances",
+  "Automative & DIY",
+  "Baby & Toddler",
+  "Books",
+  "Gaming & Media",
+  "Toys",
+];
